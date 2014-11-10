1429a844b3892d8a14a23af1e93e44c30d8ad46c
Cesar Landeros <chichozell@gmail.com>
2014-11-10 01:46:33 -0600

    home al 90%, avance en seccion de granel

--- js/app.js
+++ js/app.js
@@ -1,34 +1,40 @@
-angular.module('miApp', ['prozesa.controllers','prozesa.directives','prozesa.services','ui.bootstrap', 'ui.router'])
+angular.module('miApp', [
+    'prozesa.controllers','prozesa.directives','prozesa.services',
+        'ui.bootstrap', 'ui.router'])
 
-    .config(['$stateProvider', '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider ) {
+    .config([
+        '$stateProvider', '$urlRouterProvider',
+        function ( $stateProvider, $urlRouterProvider ) {
 
-        $urlRouterProvider.otherwise("/");
+        'use strict';
 
         $stateProvider
             .state( 'inicio', {
-                url: "/",
-                templateUrl: "bienvenido.html",
-                controller: "main"
+                url: '/',
+                templateUrl: 'bienvenido.html',
+                controller: 'main'
             })
             .state( 'inicio.granel', {
-                url: "/granel",
-                templateUrl: "prodGranel.html",
-                controller: "granel"
+                url: 'granel',
+                templateUrl: 'prodGranel.html',
+                controller: 'granel'
             })
-            .state( 'inicio.granel.producto', {
-                url: "/granel/:producto", 
-                templateUrl: "granel.html",
-                controller: "granel"
+            .state( 'inicio.granel_producto', {
+                url: 'granel/:producto',
+                templateUrl: 'granel.html',
+                controller: 'granel'
             })
             .state( 'inicio.empacados', {
-                url: "/empacados",
-                templateUrl: "prodEmp.html",
-                controller: "empacados"
+                url: 'empacados',
+                templateUrl: 'prodEmp.html',
+                controller: 'empacados'
             })
             .state( 'inicio.empacados.producto', {
-                url: "/empacados/:producto", 
-                templateUrl: "emp.html",
-                controller: "empacados"
+                url: '/empacados/:producto',
+                templateUrl: 'emp.html',
+                controller: 'empacados'
             });
-    }])
 
+            $urlRouterProvider.otherwise('/');
+
+    }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,63 +1,93 @@
 angular.module('prozesa.controllers', [])
-	.controller("main", function ($scope) {
-	  
+	.controller('main', function ($scope) {
+
+		$scope.navHeadSelected = 0;
+
 	})
-	.controller("granel", function ($scope, $routeParams, jsonRecibido) {
+	.controller('granel', function ($scope, $stateParams, jsonRecibido) {
+
+		'use strict';
+
 	    $scope.datos = jsonRecibido;
 	    $scope.direccion = {
-	        nombre: $routeParams.producto
+	        nombre: $stateParams.producto
 	    };
-	    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent){
+	    $scope.$on('ngRepeatFinished', function(){
 	        var viewPortHeight = $(window).height();
-	            $("#proGranel").height(viewPortHeight - 116);
+	            $('#proGranel').height(viewPortHeight - 116);
 	    });
 	})
-	.controller("empacados", function ($scope, $routeParams, jsonRecibido) {
+	.controller('empacados', function ($scope, $stateParams, jsonRecibido) {
+
+		'use strict';
+
 	    $scope.datos = jsonRecibido;
 	    $scope.direccion = {
-	        nombre: $routeParams.producto
+	        nombre: $stateParams.producto
 	    };
-	    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent){
+	    $scope.$on('ngRepeatFinished', function(){
 	        var viewPortHeight = $(window).height();
 	            console.log(viewPortHeight - 335);
-	            $("#imgContainer").height('auto');
-	            $("#imgContainer").height(viewPortHeight - 335);
+	            $('#imgContainer').height('auto');
+	            $('#imgContainer').height(viewPortHeight - 335);
 	    });
 	})
-	.controller("contenedor", function ($rootScope, $location) {
-	    
-	    $rootScope.$on("$routeChangeStart", function (event, current, previous, rejection) {
+	.controller('contenedor', function ($rootScope, $location) {
 
-	        if ($location.path() === "/granel" || $location.path() === ("/granel/" + current.params.producto)) {
-	            $("#labGranel").css("display", "block");
-	            $("#labEmp").css("display", "none");
+		'use strict';
+
+	    $rootScope.$on('$routeChangeStart',
+			function (event, current) {
+
+	        if ($location.path() === '/granel' ||
+				$location.path() === ( '/granel/' + current.params.producto )) {
+
+	            $('#labGranel').css('display', 'block');
+	            $('#labEmp').css('display', 'none');
+
 	        }
-	        if ($location.path() === "/empacados" || $location.path() === ("/empacados/" + current.params.producto)) {
-	            $("#labEmp").css("display", "block");
-	            $("#labGranel").css("display", "none");
+	        if ($location.path() === '/empacados' ||
+				$location.path() === ('/empacados/' + current.params.producto)){
+
+	            $('#labEmp').css('display', 'block');
+	            $('#labGranel').css('display', 'none');
+
 	        }
-	        if ($location.path() === "/") {
-	            $("#prodImg").css("display", "none");
+	        if ($location.path() === '/') {
+	            $('#prodImg').css('display', 'none');
 	        }
-	            
-	        if ($("#footer").css("bottom") === "-147px" && ($location.path() === "/granel" || $location.path() === "/empacados" || $location.path() === ("/granel/" + current.params.producto) || $location.path() === ("/empacados/" + current.params.producto))) {
-	            $("#footer").fadeOut(function () {
-	                $(this).css({ "bottom" : "auto", "top" : " 10px"});
-	                $("#prodImg").fadeIn();
+
+	        if ( $('#footer').css('bottom') === '-147px' &&
+				(
+					$location.path() === '/granel' ||
+					$location.path() === '/empacados' ||
+					$location.path() === ('/granel/' + current.params.producto) ||
+					$location.path() === ('/empacados/' + current.params.producto)
+				)) {
+
+	            $('#footer').fadeOut(function () {
+	                $(this).css({ 'bottom' : 'auto', 'top' : ' 10px'});
+	                $('#prodImg').fadeIn();
 	            });
+
 	        }
-	        if ($("#footer").css("top") === "50px" && $location.path() === "/") {
-	            $("#footer").fadeOut(function (defer) {
-	                $(this).css({ "bottom" : " -147px", "top" : "auto"});
+	        if ($('#footer').css('top') === '50px' &&
+				$location.path() === '/') {
+
+	            $('#footer').fadeOut(function () {
+	                $(this).css({ 'bottom' : ' -147px', 'top' : 'auto'});
 	            });
+
 	        }
 	    });
-	    $rootScope.$on("$routeChangeSuccess", function (event, current, previous, rejection) {
-	        $("#footer").fadeIn();
-	        
-	        if ($location.path() === "/empacados") {
-	            
+	    $rootScope.$on('$routeChangeSuccess',
+			function () {
+
+	        $('#footer').fadeIn();
+
+	        if ($location.path() === '/empacados') {
+
 	        }
 	    });
-	       
+
 	});
